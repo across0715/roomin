b947220a292cd2bfefe5aec3cf99d7fd67c2e77c
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-04-07 13:29:51 +0900

    loginするとアカウントがused　trueになる

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,23 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.update(used: "true")
+      redirect_to :index
+    else
+      render root_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :room_number, :password)
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,9 +9,21 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    # if !resource.available || resource.activated_at < Time.current
+    #   flash[:alert] = "このアカウントは無効です"
+    #   redirect_to new_user_session_path and return
+    # end
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    resource.update!(
+      used: true,
+      activated_at: Time.current + 1.week,
+    )
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
 
   # DELETE /resource/sign_out
   # def destroy
@@ -22,6 +34,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+
   # end
 end
