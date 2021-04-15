5125557d457d14dd864a8779722d4665e0495c2f
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-04-15 14:41:57 +0900

    orderページに部屋番号表示

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -2,12 +2,36 @@ ActiveAdmin.register Order do
   permit_params :user_id, :product_id, :quantity, :status, :instruct_staff_id, :instructed_staff_id
   config.sort_order = "status_asc"
   preserve_default_filters!
-  filter :status, :as => :select, :collection => [["accept", "1"], ["complete", "2"], ["unavailable", "3"]]
-  # ActiveAdmin.register_page "Order_Complete" do
-  #   belongs_to :order
-  # end
+  filter :status, as: :select, collection: Order.statuses_i18n.invert.transform_values { |v| Order.statuses[v] }
 
   actions :all, except: [:destroy]
+
+  index do
+    selectable_column
+    id_column
+    column :status, :order, &:status_i18n
+    column :room_number do |order|
+      link_to order.user.room_number, admin_user_path(order)
+    end
+    column :user
+    column :product
+    column :quantity
+    column :instruct_staff
+    column :instructed_staff
+    column :created_at
+    # column :updated_at
+    actions
+  end
+
+  # form do |f|
+  #   f.semantic_errors
+  #   f.inputs do
+
+  #     input :status, as: :select, collection: Order.statuses_i18n.invert
+
+  #   end
+  #   f.actions
+  # end
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
