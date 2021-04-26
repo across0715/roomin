c4c8603e78ff39dcfdf39e9a965b958494f2aedd
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-04-26 16:55:39 +0900

    i18n実装

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,13 @@ module HotelRoomServiceApp
 
     config.i18n.default_locale = :ja
     config.time_zone = "Asia/Tokyo"
+    # 言語ファイルを階層ごとに設定するための記述
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    # アプリケーションが対応している言語のホワイトリスト(ja = 日本語, en = 英語)
+    config.i18n.available_locales = %i(ja en zh-CN)
+    # 上記の対応言語以外の言語が指定された場合、エラーとするかの設定
+    config.i18n.enforce_available_locales = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
                        sessions: "users/sessions",
                      }
+  scope "(:locale)", locale: /#{I18n.available_locales.map(&:to_s).join("|")}/ do
+    # For details on the DSL available within this file, see
+    resources :users
+  end
+
   root to: "home#index"
 
   resources :contacts
