431f7c4ccfa601cacd2d98a754765ce40987579a
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-04-28 17:59:46 +0900

    メールの返信をユーザにできるように実装

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,7 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact_params)
     @room_number = contact_params[:room_number]
     @name = contact_params[:name]
+    @email = contact_params[:email]
     @content = contact_params[:content]
     @filename = "#{Time.current.to_i}_#{@room_number}_#{@name}.jpg"
     if contact_params[:image]
@@ -9,6 +10,9 @@ class ContactMailer < ApplicationMailer
     end
     #実際ホテルのメールアドレス
     mail to: "roomin2021@gmail.com",
+         reply_to: @email,
          subject: "お問い合わせ(#{@room_number}:#{@name})様"
   end
+
+  redirect_to root_path
 end
