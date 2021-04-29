417170d4321786bf35a6126f9972562da87b61f2
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-04-29 10:17:12 +0900

    メール送信後redirect変更

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,8 @@ class ContactsController < ApplicationController
   # POST /contacts or /contacts.json
   def create
     ContactMailer.contact_mail(contact_params).deliver_now
-    redirect_to products_path, notice: "Contact was successfully created."
+    redirect_to root_path, notice: "Contact was successfully created."
+    # t(".contact_success")
   end
 
   private
@@ -16,6 +17,6 @@ class ContactsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   # Only allow a list of trusted parameters through.
   def contact_params
-    params.require(:contact).permit(:content, :image).merge(room_number: current_user.room_number, name: current_user.name)
+    params.require(:contact).permit(:content, :image, :email).merge(room_number: current_user.room_number, name: current_user.name)
   end
 end
