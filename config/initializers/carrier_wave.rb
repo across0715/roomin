68943d28f9e49d2a90118617f97e2d0fa03455c7
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-04-29 14:03:04 +0900

    エラー解決のためにs3=>AWSに変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: "AWS",
-      aws_access_key_id: ENV["S3_ACCESS_KEY"],
-      aws_secret_access_key: ENV["S3_SECRET_KEY"],
-      region: ENV["S3_REGION"],
+      aws_access_key_id: ENV["AWS_ACCESS_KEY"],
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],
+      region: ENV["AWS_REGION"],
     }
-    config.fog_directory = ENV["S3_BUCKET"]
+    config.fog_directory = ENV["AWS_BUCKET"]
   end
 end
