c44ed78890257c7305b1f3a7c4a73b96a8d596e1
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-05-13 11:43:44 +0900

    pemit修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @product_name = Product.name
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:category, :name, :image, :avalable, :name_ja, :name_fr_FR, :name_zh_CN, name: th)
+    params.require(:product).permit(:category, :name, :image, :avalable, :name_ja, :name_fr_FR, :name_zh_CN, :name_th)
   end
 end
